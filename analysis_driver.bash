19a6ec7d6adda06ff9dd0d5b3da4adf27256a02b
Sarah Tomkovich <tomkoset@umich.edu>
2018-05-08 16:00:15 +0000

    Obtain mothur

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -1,5 +1,22 @@
 !/usr/bin/env bash
 
+Obtain linux version of mothur (V1.39.5) from the mothur GitHub repository.
+Run the following from project's root directory
+
+```bash
+wget --no-check-certificate https://github.com/mothur/mothur/releases/download/v1.39.5/Mothur.linux_64.zip
+unzip Mothur.linux_64.zip
+mv mothur code/
+rm Mothur.linux_64.zip
+rm -rf __MACOSX
+```bash
+
+Output the version of mothur
+
+```bash
+code/mothur/mothur -v
+```
+
 # Obtained the raw 'fastq.gz' files from https://www.mothur.org/MiSeqDevelopmentData.html
 # * Downloaded https://www.mothur.org/MiSeqDevelopmentData/StabilityWMetaG.tar
 # * Ran the following from the project's root directory.
